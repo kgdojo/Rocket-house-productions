c52bf44e259a8ae3ed71ba615da84e3274b154ca
Oladapo Siyanbola <siyanbololadapo@gmail.com>
2025-08-20 09:33:09 +0100

    Latest pulls

--- apps/kids-guitar-dojo/next.config.js
+++ apps/kids-guitar-dojo/next.config.js
@@ -11,21 +11,35 @@ const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
+  webpack: (config, { isServer, dev }) => {
+    if (!dev) {
+      if (isServer) {
+        config.externals.push({ html2canvas: 'commonjs html2canvas', 'video.js': 'commonjs video.js' });
+        config.plugins = [...config.plugins, new PrismaPlugin()];
+        config.devtool = 'source-map';
+      }
     }
 
     return config;
   },
+  productionBrowserSourceMaps: true,
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: true,
   },
-  transpilePackages: ['three','@react-three/fiber', '@react-three/drei'],
+  serverExternalPackages: [
+    '@prisma/client',
+    'html2canvas',
+    'video.js',
+    '@react-three/fiber',
+    '@react-three/drei',
+    'gsap',
+  ],
+  transpilePackages: ['three'],
   experimental: {
     taint: true,
+    serverSourceMaps: true,
   },
   ...(process.env.NEXT_PUBLIC_PRODUCTION && headers),
   images: {
